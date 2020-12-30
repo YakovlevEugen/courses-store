064e69bfb8d2805ea7d44ac208f05a593db35087
Yakovlev Eugen <yakovleveugen@gmail.com>
2020-12-30 15:11:05 +0300

    save sessions in mongo

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const session = require('express-session')
+const MongoStore = require('connect-mongodb-session')(session)
 const path = require('path')
 const routes = require('./routes/')
 const variablesMiddleware = require('./middleware/variables')
 
 const PORT = process.env.PORT || 3000
-const app = express()
+const MONGODB_URI = `mongodb+srv://eugen-admin:F0qQ7Fayu3nODgvO@cluster0.tcbz6.mongodb.net/demoExpress`;
 
+const app = express()
+const store = new MongoStore({
+  collection: 'sessions',
+  uri: MONGODB_URI
+})
 
 app.engine('pug', require('pug').__express)
 app.set('views', './views')
@@ -18,7 +24,8 @@ app.use(express.urlencoded({extended: false}))
 app.use(session({
   secret: 'someString',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store
 }))
 app.use(variablesMiddleware)
 routes.forEach(route => {app.use(route)})
@@ -27,10 +34,8 @@ start()
 
 
 async function start() {
-  try {
-    const dbURL = `mongodb+srv://eugen-admin:F0qQ7Fayu3nODgvO@cluster0.tcbz6.mongodb.net/demoExpress`;
-  
-    await mongoose.connect(dbURL, {
+  try {  
+    await mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false
